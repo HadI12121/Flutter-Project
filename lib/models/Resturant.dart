0abc324a5f887a6a4491c521bee2e31ac0720fab
Qasim Ikram <qasimlab118@gmail.com>
2024-05-19 19:18:25 +0500

    file added

--- lib/models/Resturant.dart
+++ lib/models/Resturant.dart
@@ -1,52 +1,182 @@
 import 'food.dart';
 
-class Resturant{
-  final List<Food> _menu= [
-    Food(name: "Classic Cheeseburger", 
-    description: "Juicy beef patty topped with melted cheddar cheese, fresh lettuce, ripe tomatoes, pickles, and our signature sauce, all nestled in a toasted sesame seed bun.", 
-    imagePath: "lib/images/burgers/", 
-    price: 0.90, 
-    category: FoodCategory.burgers, 
-    availableAddons: [
-      Addon(name: "Extra cheese", price: 0.5),
-      Addon(name: "Extra Becon", price: 0.7),
-      Addon(name: "Extra Salad", price: 0.9),
-    ]),
+class Resturant {
+  final List<Food> _menu = [
+    Food(
+        name: "Classic Cheeseburger",
+        description:
+            "Juicy beef patty topped with melted cheddar cheese, fresh lettuce, ripe tomatoes, pickles, and our signature sauce, all nestled in a toasted sesame seed bun.",
+        imagePath: "lib/images/burgers/",
+        price: 0.90,
+        category: FoodCategory.burgers,
+        availableAddons: [
+          Addon(name: "Extra cheese", price: 0.5),
+          Addon(name: "Extra Becon", price: 0.7),
+          Addon(name: "Extra Salad", price: 0.9),
+        ]),
+    Food(
+        name: "Becon Burger",
+        description:
+            "Juicy beef patty topped with melted cheddar cheese, fresh lettuce, ripe tomatoes, pickles, and our signature sauce, all nestled in a toasted sesame seed bun.",
+        imagePath: "lib/images/burgers/",
+        price: 0.90,
+        category: FoodCategory.burgers,
+        availableAddons: [
+          Addon(name: "Extra cheese", price: 0.5),
+          Addon(name: "Extra Becon", price: 0.7),
+          Addon(name: "Extra Salad", price: 0.9),
+        ]),
+    Food(
+        name: "Blue Moon Burger",
+        description:
+            "Juicy beef patty topped with melted cheddar cheese, fresh lettuce, ripe tomatoes, pickles, and our signature sauce, all nestled in a toasted sesame seed bun.",
+        imagePath: "lib/images/burgers/",
+        price: 0.90,
+        category: FoodCategory.burgers,
+        availableAddons: [
+          Addon(name: "Extra cheese", price: 0.5),
+          Addon(name: "Extra Becon", price: 0.7),
+          Addon(name: "Extra Salad", price: 0.9),
+        ]),
+    Food(
+        name: "Caesar Supreme Salad",
+        description:
+            "Crisp romaine lettuce, crunchy croutons, shaved parmesan cheese, and grilled chicken breast, all tossed in our creamy Caesar dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.salads,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
+    Food(
+        name: "Taco Salad",
+        description:
+            "Seasoned ground beef, black beans, corn, shredded lettuce, cherry tomatoes, cheddar cheese, and avocado, served in a crispy tortilla bowl with a dollop of sour cream and salsa.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.salads,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
+    Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.salads,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
 
-    Food(name: "Becon Burger", 
-    description: "Juicy beef patty topped with melted cheddar cheese, fresh lettuce, ripe tomatoes, pickles, and our signature sauce, all nestled in a toasted sesame seed bun.", 
-    imagePath: "lib/images/burgers/", 
-    price: 0.90, 
-    category: FoodCategory.burgers, 
-    availableAddons: [
-      Addon(name: "Extra cheese", price: 0.5),
-      Addon(name: "Extra Becon", price: 0.7),
-      Addon(name: "Extra Salad", price: 0.9),
-    ]),
+          Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.sides,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
 
-    Food(name: "Blue Moon Burger", 
-    description: "Juicy beef patty topped with melted cheddar cheese, fresh lettuce, ripe tomatoes, pickles, and our signature sauce, all nestled in a toasted sesame seed bun.", 
-    imagePath: "lib/images/burgers/", 
-    price: 0.90, 
-    category: FoodCategory.burgers, 
-    availableAddons: [
-      Addon(name: "Extra cheese", price: 0.5),
-      Addon(name: "Extra Becon", price: 0.7),
-      Addon(name: "Extra Salad", price: 0.9),
-    ]),
+          Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.sides,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
 
-    Food(name: "Caesar Supreme Salad", 
-    description: "Crisp romaine lettuce, crunchy croutons, shaved parmesan cheese, and grilled chicken breast, all tossed in our creamy Caesar dressing.", 
-    imagePath: "lib/images/salads/", 
-    price: 0.90, 
-    category: FoodCategory.salads, 
-    availableAddons: [
-      Addon(name: "Avocado Slices", price: 0.5),
-      Addon(name: "Shredded Cheese", price: 0.7),
-      Addon(name: "Croutons", price: 0.9),
-    ]),
+          Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.sides,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
 
+          Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.desserts,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
 
+          Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.desserts,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
 
+          Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.drinks,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
+
+          Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.drinks,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
+
+          Food(
+        name: "Kale and Apple Salad",
+        description:
+            "Tender kale leaves, thinly sliced apples, dried cranberries, walnuts, and crumbled blue cheese, tossed in a honey mustard dressing.",
+        imagePath: "lib/images/salads/",
+        price: 0.90,
+        category: FoodCategory.drinks,
+        availableAddons: [
+          Addon(name: "Avocado Slices", price: 0.5),
+          Addon(name: "Shredded Cheese", price: 0.7),
+          Addon(name: "Croutons", price: 0.9),
+        ]),
   ];
 }
